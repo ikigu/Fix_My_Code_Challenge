ab8a2d956b2cc4de827a3e187ace98679b20cf12
Ikigu <ikigu.geo@gmail.com>
2024-03-22 18:14:52 +0300

    fix: delete correct node

--- 4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -19,18 +19,22 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		return (-1);
 	}
+
 	saved_head = *head;
 	p = 0;
+
 	while (p < index && *head != NULL)
 	{
 		*head = (*head)->next;
 		p++;
 	}
+
 	if (p != index)
 	{
 		*head = saved_head;
 		return (-1);
 	}
+
 	if (0 == index)
 	{
 		tmp = (*head)->next;
@@ -43,10 +47,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
+		(*head)->prev->next = (*head)->next;
+		
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
+		
+		free(*head);
 		*head = saved_head;
 	}
 	return (1);
